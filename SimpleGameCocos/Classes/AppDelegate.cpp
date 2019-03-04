d9c403199423a42cf8e090d18dde349359d9d4d6
Radu Albastroiu <albastroiuradug@gmail.com>
2019-03-04 22:05:08 +0200

    Changed screen resolutions

--- SimpleGameCocos/Classes/AppDelegate.cpp
+++ SimpleGameCocos/Classes/AppDelegate.cpp
@@ -47,6 +47,10 @@ static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
 static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
 
+static cocos2d::Size smallResolution169 = cocos2d::Size(1366, 768);
+static cocos2d::Size mediumResolution169 = cocos2d::Size(1920, 1080);
+static cocos2d::Size largeResolution169 = cocos2d::Size(2560, 1440);
+
 AppDelegate::AppDelegate()
 {
 }
@@ -81,9 +85,12 @@ bool AppDelegate::applicationDidFinishLaunching() {
     // initialize director
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
+
+    auto resolution = largeResolution169;
+
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("SimpleGameCocos", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("SimpleGameCocos", cocos2d::Rect(0, 0, resolution.width, resolution.height));
 #else
         glview = GLViewImpl::create("SimpleGameCocos");
 #endif
@@ -95,25 +102,6 @@ bool AppDelegate::applicationDidFinishLaunching() {
 
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
-
-    // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
-    auto frameSize = glview->getFrameSize();
-    // if the frame's height is larger than the height of medium size.
-    if (frameSize.height > mediumResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is larger than the height of small size.
-    else if (frameSize.height > smallResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is smaller than the height of medium size.
-    else
-    {        
-        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
-    }
 
     register_all_packages();
 
