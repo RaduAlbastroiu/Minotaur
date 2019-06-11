ebb2fe1782d1c872531b5e4cb8a7f5df24906170
Radu Albastroiu <albastroiuradug@gmail.com>
2019-06-11 21:16:48 +0300

    Update

---
+++ SimpleGameCocos/Classes/Includes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <map>
+
+#define LEFT 1
+#define RIGHT 2
+#define UP 3
+#define DOWN 4
+#define NODIRECTION -1
+
+#define HERO_ATTACK_RADIUS 50
+
+using namespace std;
